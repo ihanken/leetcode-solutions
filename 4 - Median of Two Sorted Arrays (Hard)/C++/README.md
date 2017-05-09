50c2cee02979ee97118cbc97348e0b0b0afb37c4
ihanken <ihanken@bellsouth.net>
2017-05-08 18:23:20 -0700

    Refined a bit.

--- 4 - Median of Two Sorted Arrays (Hard)/C++/README.md
+++ 4 - Median of Two Sorted Arrays (Hard)/C++/README.md
@@ -15,4 +15,5 @@ numbers given is larger than the second (switch the arrays if needed). Then, the
 arrays will be cut be repeatedly using a binary search for low and high values of
 each array. If a point is ever found in which both low values are less than both
 high values, simply average the max of the low values with the min of the high
-values. This is the answer.
+values. This is the answer. Note that the values you start with should be the
+middle values of each array, and then you should expand outwards.
--- 4 - Median of Two Sorted Arrays (Hard)/C++/solution.cpp
+++ 4 - Median of Two Sorted Arrays (Hard)/C++/solution.cpp
@@ -18,16 +18,14 @@ public:
             int mid2 = (low + high) / 2;
             int mid1 = size1 + size2 - mid2;
 
-            double L1 = (mid1 == 0) ? INT_MIN : nums1[(mid1 - 1) / 2];      // Get the low index of nums1.
-            double L2 = (mid2 == 0) ? INT_MIN : nums2[(mid2 - 1) / 2];      // Get the low index of nums2.
-            double R1 = (mid1 == size1 * 2) ? INT_MAX : nums1[(mid1) / 2];  // Get the high index of nums1.
-            double R2 = (mid2 == size2 * 2) ? INT_MAX : nums2[(mid2) / 2];  // Get the high index of nums2.
+            double L1 = (mid1 == 0) ? INT_MIN : nums1[(mid1 - 1) / 2]; // Low of nums1.
+            double L2 = (mid2 == 0) ? INT_MIN : nums2[(mid2 - 1) / 2]; // Low of nums2.
+            double R1 = (mid1 == size1 * 2) ? INT_MAX : nums1[(mid1) / 2]; // High of nums1.
+            double R2 = (mid2 == size2 * 2) ? INT_MAX : nums2[(mid2) / 2]; // High of nums2.
 
-            cout << L1 << ", " << L2 << ", " << R1 << ", " << R2 << endl;
-
-            if (L1 > R2) low = mid2 + 1;		// num1's lower half is too big, so move our low up.
-            else if (L2 > R1) high = mid2 - 1;	// num2's lower half is too big, so move our high down.
-            else return (max(L1,L2) + min(R1, R2)) / 2;	// Otherwise, we can return.
+            if (L1 > R2) low = mid2 + 1;		// num1's lower half is too big.
+            else if (L2 > R1) high = mid2 - 1;	// num2's lower half is too big.
+            else return (max(L1,L2) + min(R1, R2)) / 2;	// Otherwise, return.
         }
 
         return (double) -1; // Shouldn't ever get here.
