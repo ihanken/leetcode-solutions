67ec5baf1f13316e6e2c9cf35277c96ffbacafd1
ihanken <ihanken@bellsouth.net>
2017-05-08 18:20:07 -0700

    Added a C++ solution for the median of two sorted arrays.

---
+++ 4 - Median of Two Sorted Arrays (Hard)/C++/README.md
@@ -0,0 +1,18 @@
+# C++ Solution for 4 - Median of Two Sorted Arrays
+
+## Approach
+
+The median of any set of numbers can be arrived at by splitting the set of
+numbers into two number sets such that the sizes of the sets are equal and all of
+the numbers in the first set are smaller than all of the numbers in the second set.
+Then, you simply average the minimum from the first set with the maximum of the
+second set. Thus, we simply need to implement a method two find these two sets.
+
+## Explanation
+
+To do this we will use binary search. First, make sure that the first array of
+numbers given is larger than the second (switch the arrays if needed). Then, the
+arrays will be cut be repeatedly using a binary search for low and high values of
+each array. If a point is ever found in which both low values are less than both
+high values, simply average the max of the low values with the min of the high
+values. This is the answer.
---
+++ 4 - Median of Two Sorted Arrays (Hard)/C++/solution.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if (nums1.size() < nums2.size()) { // Make nums2 the shorter one.
+            vector<int> tmp = nums1;
+            nums1 = nums2;
+            nums2 = tmp;
+        }
+
+        int size1 = nums1.size();
+        int size2 = nums2.size();
+
+        // If the smaller array is empty, just find the median of the larger array.
+        if (nums2.size() == 0) return ((double) nums1[(size1 - 1) / 2] + (double) nums1[size1 / 2]) / 2;
+
+        int low = 0, high = size2 * 2;
+        while (low <= high) {
+            int mid2 = (low + high) / 2;
+            int mid1 = size1 + size2 - mid2;
+
+            double L1 = (mid1 == 0) ? INT_MIN : nums1[(mid1 - 1) / 2];      // Get the low index of nums1.
+            double L2 = (mid2 == 0) ? INT_MIN : nums2[(mid2 - 1) / 2];      // Get the low index of nums2.
+            double R1 = (mid1 == size1 * 2) ? INT_MAX : nums1[(mid1) / 2];  // Get the high index of nums1.
+            double R2 = (mid2 == size2 * 2) ? INT_MAX : nums2[(mid2) / 2];  // Get the high index of nums2.
+
+            cout << L1 << ", " << L2 << ", " << R1 << ", " << R2 << endl;
+
+            if (L1 > R2) low = mid2 + 1;		// num1's lower half is too big, so move our low up.
+            else if (L2 > R1) high = mid2 - 1;	// num2's lower half is too big, so move our high down.
+            else return (max(L1,L2) + min(R1, R2)) / 2;	// Otherwise, we can return.
+        }
+
+        return (double) -1; // Shouldn't ever get here.
+    }
+};
